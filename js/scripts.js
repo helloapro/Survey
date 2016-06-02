3e166f184406d150203f66f0688647dcb572a21d
April Peng and Alex Zerbach <alex.zerbach@gmail.com>
2016-06-01 21:08:44 -0700

    add dob to survey. update format and styling further for values and submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-$(function(){
+$(function() {
   $("#blanks form").submit(function(event){
     var nameInput = $("input#name").val();
+    var date = $("#date").val();
     var color = $("input:radio[name=color]:checked").val();
     var music = $("#music").val();
 
     $(".name").text(nameInput);
+    $(".date").text(date);
     $(".color").text(color);
     $(".music").text(music);
 
